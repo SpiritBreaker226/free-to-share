61593b8fbc71d96d110eabd574b918f614ad8a41
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-17 14:50:10 -0700

    Add Car Item Component
    Add the Car Item component to display the cars found to the user.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+
+import { CarItem } from './Components'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <CarItem
+        car={{
+          id: 1,
+          make: 'Mitsubishi',
+          model: 'Montero',
+          color: 'Yellow',
+          model_year: 2002,
+          vin: 'SAJWJ0FF3F8321657',
+          price: '$2814.46',
+          availability: false,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +30,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
