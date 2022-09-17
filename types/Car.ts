50c3d6c3c7f4c92b87f015f2d7b5702c59e2f715
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-17 14:46:51 -0700

    Add Car Type
    Add a Car type that will hold what the car details will be for so that
    the developer can see what is in the item.

---
+++ types/Car.ts
@@ -0,0 +1,11 @@
+export type Car = {
+  id: number
+  make: string
+  model: string
+  color: string
+  model_year: number
+  vin: string
+  price: string
+  availability: boolean
+  imageSource?: string
+}
