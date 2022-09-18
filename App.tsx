814456207ec3a507e6df7ade624d120708822cec
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-18 15:18:37 -0700

    Remove Cart Item, No Longer Needed
    Remove the cart item since it is no longer needed as a List Item from
    React Native Paper will be used for the look and feel of the search
    items.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { Provider as PaperProvider } from 'react-native-paper'
 
 import { AppBody } from './AppBody'
-import { CarItem, Search } from './Components'
+import { Search } from './Components'
 import { AppProvider } from './contexts'
 import { theme } from './theme'
 
@@ -10,19 +10,6 @@ export const App = () => {
     <AppProvider>
       <PaperProvider theme={theme}>
         <Search />
-
-        <CarItem
-          car={{
-            id: 1,
-            make: 'Mitsubishi',
-            model: 'Montero',
-            color: 'Yellow',
-            model_year: 2002,
-            vin: 'SAJWJ0FF3F8321657',
-            price: '$2814.46',
-            availability: false,
-          }}
-        />
 
         <AppBody />
       </PaperProvider>
--- Components/index.ts
+++ Components/index.ts
@@ -1,2 +1,1 @@
-export * from './CarItem'
 export * from './Search'
