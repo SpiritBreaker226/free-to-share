221bade3884741a3933779081d20029291943735
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-18 19:22:07 -0700

    Add Filter Types
    Add filter types to ensure that the correct filter is used to search for
    this instance.

---
+++ types/FilterTypes.ts
@@ -0,0 +1,6 @@
+export enum FilterTypes {
+  MAKE = 'make',
+  YEAR = 'model_year',
+  MODEL = 'model',
+  COLOR = 'color',
+}
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,5 @@
 export * from './Actions'
 export * from './Car'
 export * from './CarFromApi'
+export * from './FilterTypes'
 export * from './ResponseFromCarApi'
