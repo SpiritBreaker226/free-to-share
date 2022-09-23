8f12c63c085e53e4eec46b0e03e2e99bd8df9ba2
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-23 12:01:56 -0700

    Add Image & Move List Item to Car Item
    Add an image for each car and move the List Item to the Car Item
    component. As the List Item provides the layout to display on mobile,
    the Car Item helps clean up the Car List component so that it can focus
    on the list part of the application.

--- AppBody/Components/CarList.tsx
+++ AppBody/Components/CarList.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useCallback, useState } from 'react'
 import { FlatList, SafeAreaView } from 'react-native'
-import { List, Text } from 'react-native-paper'
 
+import { CarItem } from '../../Components'
 import { Car } from '../../models'
 
 export type CarListProps = {
@@ -12,21 +12,7 @@ export const CarList: FC<CarListProps> = ({ cars }) => {
   const [numberOfCars, setNumberOfCars] = useState(24)
 
   const renderItem = useCallback(
-    ({ item }: { item: Car & Realm.Object }) => (
-      <List.Section>
-        <List.Item
-          title={item.make}
-          description={
-            <>
-              <Text>Year: {item.model_year} </Text>
-              <Text>Color: {item.color} </Text>
-              <Text>Price: {item.price}</Text>
-            </>
-          }
-          left={(props) => <List.Icon {...props} icon="folder" />}
-        />
-      </List.Section>
-    ),
+    ({ item }: { item: Car & Realm.Object }) => <CarItem car={item} />,
     []
   )
 
--- Components/CarItem.tsx
+++ Components/CarItem.tsx
@@ -3,7 +3,7 @@ import FastImage from 'react-native-fast-image'
 import React, { FC } from 'react'
 
 import { Car } from '../models'
-import { Headline, Text } from 'react-native-paper'
+import { List, Text } from 'react-native-paper'
 
 export type CarItemProps = {
   car: Car & Realm.Object
@@ -11,59 +11,35 @@ export type CarItemProps = {
 
 export const CarItem: FC<CarItemProps> = ({ car }) => {
   return (
-    <View style={styles.container}>
-      {car.imageSource ? (
-        <Image
-          style={styles.img}
-          source={{ uri: 'https://picsum.photos/id/1071/200/200' }}
-          resizeMode={'cover'}
-        />
-      ) : (
-        // placeholder
-        // If you see a continental front bumper then it is the placeholder
-        <Image
-          style={styles.img}
-          source={{
-            uri: 'https://i.picsum.photos/id/1071/200/200.jpg?hmac=mb6el6MCnRCyFnuMcCPJppn1WISnV5OKFUqDFg82Joo',
-          }}
-          resizeMode={'cover'}
-        />
-      )}
-      <View>
-        <Headline style={styles.make}>{car.make}</Headline>
-        <View style={styles.details}>
-          <Text>Model: {car.model}</Text>
-          <Text>Year: {car.model_year}</Text>
-          <Text>Color: {car.color}</Text>
+    <List.Item
+      title={car.make}
+      description={
+        <>
+          <Text>Year: {car.model_year} </Text>
+          <Text>Color: {car.color} </Text>
           <Text>Price: {car.price}</Text>
-        </View>
-      </View>
-    </View>
+        </>
+      }
+      left={() =>
+        car.imageSource ? (
+          <FastImage style={styles.img} source={{ uri: car.imageSource }} />
+        ) : (
+          // placeholder
+          // If you see a continental front bumper then it is the placeholder
+          <FastImage
+            style={styles.img}
+            source={{ uri: 'https://picsum.photos/id/1071/200' }}
+          />
+        )
+      }
+    />
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // backgroundColor: '#999',
-    // color: '#fff',
-    // border: '1px solid #000',
-    padding: 16,
-    marginBottom: 8,
-    display: 'flex',
-  },
   img: {
-    // border: '1px solid #000',
     borderRadius: 4,
-    marginRight: 8,
     width: 50,
     height: 50,
-  },
-  make: {
-    marginTop: 0,
-    marginBottom: 16,
-  },
-  details: {
-    display: 'flex',
-    gap: 16,
   },
 })
